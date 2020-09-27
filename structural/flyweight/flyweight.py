281b712fc3ff792ca56c41cf8441f92cda04730b
Ruidy <r.nemausat@empfohlen.de>
2020-09-27 21:22:43 +0200

    add code example

---
+++ structural/flyweight/flyweight.py
@@ -0,0 +1,20 @@
+import json
+from typing import List
+
+
+class Flyweight:
+    """
+    The Flyweight stores a common portion of the state (also called intrinsic
+    state) that belongs to multiple real business entities. The Flyweight
+    accepts the rest of the state (extrinsic state, unique for each entity) via
+    its method parameters.
+    """
+
+    def __init__(self, shared_state: List[str]) -> None:
+        self._shared_state = shared_state
+
+    def operation(self, unique_state: List[str]) -> None:
+        s = json.dumps(self._shared_state)
+        u = json.dumps(unique_state)
+        print(f"Flyweight: Displaying shared ({s}) and unique ({u}) state.",
+              end="")
---
+++ structural/flyweight/main.py
@@ -0,0 +1,37 @@
+from structural.flyweight.flyweight_factory import FlyweightFactory
+
+
+def add_car_to_police_database(factory: FlyweightFactory, plates: str,
+                               owner: str, brand: str, model: str,
+                               color: str) -> None:
+
+    print("\n\nClient: Adding a car to database.")
+    flyweight = factory.get_flyweight([brand, model, color])
+    # The client code either stores or calculates extrinsic state and passes it
+    # to the flyweight's methods.
+    flyweight.operation([plates, owner])
+
+
+if __name__ == '__main__':
+    """
+    The client code usually creates a bunch of pre-populated flyweights in the
+    initialization stage of the application.
+    """
+    factory = FlyweightFactory([
+        ["Chevrolet", "Camaro2018", "pink"],
+        ["Mercedes Benz", "C300", "black"],
+        ["Mercedes Benz", "C500", "red"],
+        ["BMW", "M5", "red"],
+        ["BMW", "X6", "white"],
+    ])
+
+    factory.list_flyweights()
+
+    add_car_to_police_database(
+        factory, "CL234IR", "James Doe", "BMW", "M5", "red")
+    add_car_to_police_database(
+        factory, "CL234IR", "James Doe", "BMW", "X1", "red")
+
+    print("\n")
+
+    factory.list_flyweights()
