ca672add42e7b04ea458d7be7e405acbf9b07c05
Ruidy <nemausatpro@gmail.com>
2020-10-09 14:07:14 +0200

    Template (#23)
    * add documentation
    
    * add code example

---
+++ behavioral/template/__init__.py
@@ -0,0 +1,4 @@
+"""
+Template Method is a behavioral design pattern that allows you to defines a skeleton of an algorithm in a base class and
+let subclasses override the steps without changing the overall algorithm’s structure.
+"""
